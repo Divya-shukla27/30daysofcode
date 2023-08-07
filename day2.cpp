e7955a41b0d9b947667aeeb1682c0bf3e61548d2
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-07 22:28:57 +0530

    Add files via upload

---
+++ day2.cpp
@@ -0,0 +1,57 @@
+/*Task
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+Use the  operator to perform the following operations:
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+Note: If you are using a language that doesn't support using  for string concatenation (e.g.: C), you can just print one variable immediately following the other on the same line. The string provided in your editor must be printed first, immediately followed by the string you read as input.
+
+Input Format
+
+The first line contains an integer that you must sum with .
+The second line contains a double that you must sum with .
+The third line contains a string that you must concatenate with .
+
+Output Format
+
+Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a=4;
+    double b=4.0;
+    string c="Hackerank ";
+    int i;
+    double j;
+    string k;
+
+    cin>>i>>j;
+    cin.ignore();
+    getline(cin,k);
+    cout<<a+i<<endl;
+    cout<<fixed<<setprecision(1)<<b+j<<endl;
+    cout<<c+k<<endl;
+}
