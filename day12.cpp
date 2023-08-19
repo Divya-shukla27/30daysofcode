fb1be8043a28a0e9f5fa180e56e8b57297e693b3
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-19 18:10:16 +0530

    Add files via upload

---
+++ day12.cpp
@@ -0,0 +1,155 @@
+/*
+Context
+Given a  2D Array, :
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+a b c
+  d
+e f g
+There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is  for the hourglass in the top left corner.
+
+Input Format
+
+There are  lines of input, where each line contains  space-separated integers that describe the 2D Array .
+
+Constraints
+
+Output Format
+
+Print the maximum hourglass sum in .
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+Sample Output
+
+19
+Explanation
+
+ contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+The hourglass with the maximum sum () is:
+
+2 4 4
+  2
+1 2 4
+*/
+#include <bits/stdc++.h>
+#include<climits>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int hourglass(vector<vector<int>> &arr) {
+    int ma = INT_MIN;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            int ans = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] +
+                      arr[i + 1][j + 1] +
+                      arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+            ma = max(ans, ma);
+        }
+    }
+    return ma;
+}
+
+int main()
+{
+
+    vector<vector<int>> arr(6);
+
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < 6; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+    int out = hourglass(arr);
+    cout<<out<<endl;
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
